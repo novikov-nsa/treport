2800c5b0727d2cca59752f08d7b130ee35527c74
novikov.sergey <novikov.sergey@otr.ru>
2022-05-21 10:55:12 +0300

    #1 Реализован минимально работающий прототип. README.md в процессе написания.

---
+++ treport_example/run_report.py
@@ -0,0 +1,10 @@
+from treport.report import Report, get_config
+
+if __name__ == '__main__':
+    parameters = {'p_start_date': '01.05.2022', 'p_end_date': '31.05.2022'}
+    db_url, path_to_params_reports_file = get_config('run_report.ini')
+    report = Report('treport_example', path_to_params_reports_file, parameters, db_url)
+
+    if report.xml_validation_result:
+        report.contentReport.save(report.outDir + report.report_file_name)
+        report.logger.info(f'Файл {report.outDir + report.report_file_name} сохранен')
