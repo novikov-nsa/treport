2d4e9387ce2b7b034f0b30f45f2249b70a2a74b7
novikov.sergey <novikov.sergey@otr.ru>
2022-06-15 17:29:09 +0300

    #7 Реализована возможность выводить значения параметров в заголовке отчета

--- treport_example/sql/treport_example_page1.sql
+++ treport_example/sql/treport_example_page1.sql
@@ -1,3 +1,3 @@
 select d.docnum, to_char(d.docdate, 'dd.mm.yyyy'), d.docsum, d.docsum2  from treport.documents d
-where d.docdate between symmetric to_date('{{ p_start_date }}', 'dd.mm.yyyy') and to_date('{{ p_end_date }}', 'dd.mm.yyyy')
+where d.docdate between to_date('{{ p_start_date }}', 'dd.mm.yyyy') and to_date('{{ p_end_date }}', 'dd.mm.yyyy')
 order by d.docdate asc;
